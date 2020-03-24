4e391f61dcb742a0605e7197b78addbad86185d2
Kenneth Thorø Martinsen <kenneth2810@gmail.com>
2020-03-24 10:41:01 +0100

    Identified zero-catch sampling and delineated lake basins.

--- data_raw.R
+++ data_raw.R
@@ -55,9 +55,11 @@ ogr2ogr(paste0(getwd(), "/data_raw/DK_WatercourseLink.gml"),
 
 #Read downloaded fish data, clean and save to gis_database
 #Format to columns: system, site_id, year, Xutm_Euref89_Zone32, Yutm_Euref89_Zone32, name_local
-fish_lake_raw <- read_xlsx(paste0(getwd(), "/data_raw/", "odaforalle_fish_lake.xlsx"))
+#For lake fish monitoring both weigth and net tables are needed to also include zero-catch samples
+fish_weight_lake <- read_xlsx(paste0(getwd(), "/data_raw/", "odaforalle_fish_weight_lake.xlsx"))
+fish_net_lake <- read_xlsx(paste0(getwd(), "/data_raw/", "odaforalle_fish_net_lake.xlsx"))
 
-fish_lake_species <- fish_lake_raw %>% 
+fish_lake_species_raw <- left_join(fish_net_lake, fish_weight_lake) %>% 
   mutate(system = "lake", date = ymd(Dato), year = year(date),
          Xutm_Euref89_Zone32 = as.numeric(Xutm_Euref89_Zone32), Yutm_Euref89_Zone32 = as.numeric(Yutm_Euref89_Zone32)) %>% 
   select(system, site_id = ObservationsStedNr, year,
@@ -65,15 +67,39 @@ fish_lake_species <- fish_lake_raw %>%
   distinct() %>% 
   filter(!is.na(Xutm_Euref89_Zone32))
 
-fish_stream_raw <- read_xlsx(paste0(getwd(), "/data_raw/", "odaforalle_fish_stream.xlsx"))
+keep_na_for_zero_catch <- function(spec_col){
+  if(all(is.na(spec_col[[1]]))){
+    df <- tibble(col_name = NA)
+    names(df) <- names(spec_col)
+    return(df)
+  }else{
+    return(na.omit(spec_col))
+  }
+}
 
+fish_lake_species <- fish_lake_species_raw %>% 
+  nest(data = c("name_local")) %>% 
+  mutate(na_if_zerocatch = map(data, keep_na_for_zero_catch)) %>% 
+  select(-data) %>% 
+  unnest(na_if_zerocatch)
+
+# #Count number of fish caught for each sampling
+# fish_lake_species_count <- fish_lake_species %>% 
+#   group_by(system, site_id, Xutm_Euref89_Zone32, Yutm_Euref89_Zone32, year) %>% 
+#   summarise(n_spec = ifelse(any(is.na(name_local)), 0, length(unique(name_local)))) %>% 
+#   ungroup()
+
+#Stream fish, used to define basin species pool
+fish_stream_raw <- read_xlsx(paste0(getwd(), "/data_raw/", "odaforalle_fish_stream.xlsx"))
 fish_stream_species <- fish_stream_raw %>% 
   mutate(system = "stream", date = ymd(Dato), year = year(date),
          Xutm_Euref89_Zone32 = as.numeric(Xutm_Euref89_Zone32), Yutm_Euref89_Zone32 = as.numeric(Yutm_Euref89_Zone32)) %>% 
   select(system, site_id = ObservationsStedNr, year,
          Xutm_Euref89_Zone32, Yutm_Euref89_Zone32, name_local = Fiskart) %>% 
-  distinct()
+  distinct() %>% 
+  na.omit()
 
+#Fish in new lakes
 fish_newlakes_raw <- read_xlsx(paste0(getwd(), "/data_raw/", "Samlet arter nye søer.xlsx")) %>% 
   mutate(system = "newlake", site_id = group_indices(., Lokalitetsnavn))
   
@@ -96,11 +122,10 @@ fish_newlakes <- bind_rows(fish_newlakes_raw_zone32, fish_newlakes_raw_zone33_to
   distinct() %>% 
   mutate(year = 2018) #Avg year of sampling in new lakes
 
-fish_species <- bind_rows(fish_lake_species, fish_stream_species, fish_newlakes) %>% 
-  mutate(name_local = ifelse(is.na(name_local), "none", name_local))
+fish_species <- bind_rows(fish_lake_species, fish_stream_species, fish_newlakes)
 
 #Identify fish species for further analysis
-fish_unique <- fish_species %>% distinct(name_local) %>% arrange(name_local)
+fish_unique <- fish_species %>% distinct(name_local) %>% arrange(name_local) %>% na.omit()
 write_csv(fish_unique, paste0(getwd(), "/data_raw/", "fish_unique.csv"))
 
 #Select fish species for further analysis
@@ -109,16 +134,24 @@ fish_unique_edit <- read_xlsx(paste0(getwd(), "/data_raw/", "fish_unique_edit.xl
   filter(keep == "y") %>% 
   select(name_local, fish_id = ID)
 
-#Join to table
+#Join to table with species id's, keep "y" fish and NA
 fish_species_sub <- fish_species %>% 
-  right_join(fish_unique_edit) %>% 
+  left_join(fish_unique_edit) %>% 
+  filter(name_local %in% c(fish_unique_edit$name_local, NA)) %>% 
   select(-name_local, -year_established)
 
 #Basin fish species pool is all species in streams and lakes found since 1990
 fish_species_basin <- fish_species_sub %>% 
   filter(year > 1990) %>% 
   select(-year) %>% 
+  na.omit() %>% 
   distinct()
+
+#Write basin species data to gis database
+fish_species_basin_sf <- fish_species_basin %>% 
+  st_as_sf(coords = c("Xutm_Euref89_Zone32", "Yutm_Euref89_Zone32"), crs = dk_epsg)
+
+st_write(fish_species_basin_sf, dsn = gis_database, layer = "fish_species_basin", delete_layer = TRUE)
 
 #Lakes for investigation is all the latest sampling for each lakes after 2006
 fish_species_latest_survey <- fish_species_sub %>% 
@@ -132,19 +165,64 @@ fish_species_latest_survey <- fish_species_sub %>%
 #Lakes for further investigation
 fish_species_lakes <- fish_species_latest_survey %>% 
   left_join(fish_species_sub, by = c("system" = "system", "site_id" = "site_id", "year_max" = "year")) %>% 
-  select(-year_max) %>% 
+  rename(year = year_max) %>% 
   distinct()
   
-#Write basin and lake fish species to gis_database
-fish_species_basin_sf <- fish_species_basin %>% 
-  st_as_sf(coords = c("Xutm_Euref89_Zone32", "Yutm_Euref89_Zone32"), crs = dk_epsg)
-
-st_write(fish_species_basin_sf, dsn = gis_database, layer = "fish_species_basin", delete_layer = TRUE)
-
+#Write basin species data to gis database
 fish_species_lakes_sf <- fish_species_lakes %>% 
   st_as_sf(coords = c("Xutm_Euref89_Zone32", "Yutm_Euref89_Zone32"), crs = dk_epsg)
 
 st_write(fish_species_lakes_sf, dsn = gis_database, layer = "fish_species_lakes", delete_layer = TRUE)
 
+#Secchi depth data 
+lake_secchi <- read_xlsx(paste0(getwd(), "/data_raw/", "odaforalle_secchi_lake.xlsx")) %>% 
+  mutate(system = "lake", date = ymd(Startdato), year = year(date),
+         Xutm_Euref89_Zone32 = as.numeric(Xutm_Euref89_Zone32_ZONE32), 
+         Yutm_Euref89_Zone32 = as.numeric(Yutm_Euref89_Zone32_ZONE32)) %>% 
+  select(system, site_id = ObservationsStedNr, year,
+         Xutm_Euref89_Zone32, Yutm_Euref89_Zone32, secchi_depth_m = Resultat) %>% 
+  filter(!is.na(Xutm_Euref89_Zone32),
+         Yutm_Euref89_Zone32 > 700000,
+         secchi_depth_m < 20,
+         year >= 2006) #Remove one errornous value 
 
-#Lake macrophyte, secchi depth and chemistry?? - load, clean and write to gis_database
+lake_secchi_sf <- lake_secchi %>% 
+  st_as_sf(coords = c("Xutm_Euref89_Zone32", "Yutm_Euref89_Zone32"), crs = dk_epsg)
+
+st_write(lake_secchi_sf, dsn = gis_database, layer = "lake_secchi", delete_layer = TRUE)
+
+#Chemistry data
+#Consider filter more on outliers in vars e.g. alkalinity
+lake_chem <- read_xlsx(paste0(getwd(), "/data_raw/", "odaforalle_chemistry_lake.xlsx")) %>% 
+  mutate(system = "lake", date = ymd(Startdato), year = year(date),
+         Xutm_Euref89_Zone32 = as.numeric(Xutm_Euref89_Zone32), 
+         Yutm_Euref89_Zone32 = as.numeric(Yutm_Euref89_Zone32),
+         Parameter_cor = ifelse(Parameter == "Chlorophyl (ukorrigeret)", "Chlorophyl A", Parameter),
+         var_unit = paste0(Parameter_cor, "_", Enhed),
+         avg_sample_depth_m = parse_number(`GennemsnitsDybde i m`, locale = locale(decimal_mark = ","))) %>% 
+  select(system, site_id = ObservationsStedNr, date, year, Xutm_Euref89_Zone32, Yutm_Euref89_Zone32, 
+         avg_sample_depth_m, var_unit, value = Resultat) %>% 
+  group_by(system, site_id, date, year, Xutm_Euref89_Zone32, Yutm_Euref89_Zone32, var_unit) %>% #Average across depth
+  summarise(value_mean = mean(value)) %>% 
+  spread(var_unit, value_mean) %>% 
+  rename(alk_mmol_l = `Alkalinitet,total TA_mmol/l`, chla_ug_l = `Chlorophyl A_µg/l`, 
+         tn_mg_l = `Nitrogen,total N_mg/l`, ph_ph = pH_pH, tp_mg_l = `Phosphor, total-P_mg/l`) %>% 
+  group_by(system, site_id, Xutm_Euref89_Zone32, Yutm_Euref89_Zone32, year) %>% 
+  summarise_at(vars(alk_mmol_l, chla_ug_l, tn_mg_l, ph_ph, tp_mg_l),
+               ~weighted.mean(., w = yday(date), na.rm = TRUE)) %>% #Weighted mean across year
+  ungroup() %>% 
+  filter(Yutm_Euref89_Zone32 > 700000,
+         year >= 2006)
+
+lake_chem_sf <- lake_chem %>% 
+  st_as_sf(coords = c("Xutm_Euref89_Zone32", "Yutm_Euref89_Zone32"), crs = dk_epsg)
+
+st_write(lake_chem_sf, dsn = gis_database, layer = "lake_chem", delete_layer = TRUE)
+
+
+
+
+
+
+
+#Lake macrophyte??
--- gis_grass_processing.R
+++ gis_grass_processing.R
@@ -15,6 +15,7 @@ link2GI::linkGRASS7(dk_dem_25,
                     gisdbase = paste0(getwd(), "/gis_grass_database"), 
                     location = "dk_25",
                     gisdbase_exist = TRUE)
+use_sp()
 
 #Import dem
 execGRASS("r.in.gdal", flags = c("o"), parameters = list(input = dk_dem_25_path, output = "dem"))
@@ -45,36 +46,90 @@ execGRASS("r.to.vect", flags = c("s", "overwrite"),
                             type = "area"))
 
 #Import vector file to R
-use_sp()
 dem_basins_sp <- readVECT("dem_basins")
 
 dem_basins_sf <- dem_basins_sp %>% 
   st_as_sf() %>% 
-  select(-cat) %>% 
-  mutate(area = as.numeric(st_area(geometry))) %>% 
-  filter(area>625) %>% 
-  group_by(value) %>% 
-  summarise() %>% 
   select(basin_id = value) %>% 
+  group_by(basin_id) %>% 
+  summarise() %>% 
   st_cast("MULTIPOLYGON") %>% 
   st_set_crs(dk_epsg)
 
-st_write(dem_basins_sf, dsn = gis_database, layer = "basins", delete_layer = TRUE)
+st_write(dem_basins_sf, dsn = gis_database, layer = "dk_basins", delete_layer = TRUE)
 
 #Delineate lake watersheds
+#Find lakes with fish species data
+dk_lakes <- st_read(dsn = gis_database, layer = "dk_lakes")
+fish_species_lakes <- st_read(dsn = gis_database, layer = "fish_species_lakes")
 
-
-
-
-#join fish and lakes, extract the polys, export to grass, delineate watersheds, export and collect in R, write to database
-
-lakes <- st_read(dsn = gis_database, layer = "dk_lakes")
-fish <- st_read(dsn = gis_database, layer = "fish_species_lakes")
-
-tmp <- fish %>% 
+fish_species_lakes_polys <- fish_species_lakes %>% 
   select(system, site_id) %>% 
-  distinct(.keep_all = TRUE) %>% 
-  st_join(lakes, left = FALSE) 
+  distinct(.keep_all = TRUE) %>% #300 lakes with fish data
+  st_join(dk_lakes, left = FALSE) #Polygons for 287 site_ids but only 278 polygons - some polygons are shared between site_id's
 
-pull(gml_id)
+#Extract lake polygons and export to grass
+dk_lakes_sub <- dk_lakes %>% 
+  filter(gml_id %in% fish_species_lakes_polys$gml_id) %>% 
+  mutate(lake_basin_id = 1:n())
+
+writeVECT(as(dk_lakes_sub[, "lake_basin_id"], "Spatial"), "dk_lakes_sub", v.in.ogr_flags = "overwrite")
+
+#Delineate watersheds using gml_id for lake polygons id's
+lake_basin_indexes <- dk_lakes_sub$lake_basin_id
+for(i in lake_basin_indexes){
+  print(paste0("Creating watershed for lake_basin_id ", i, " or gml_id ", dk_lakes_sub$gml_id[i]))
+  
+  execGRASS("v.extract", flags = c("overwrite"), parameters = list(input = "dk_lakes_sub@PERMANENT",
+                                                                   output = paste0("tmp_", i),
+                                                                   type = "area",
+                                                                   layer = "1",
+                                                                   new = -1,
+                                                                   where = paste0("lake_basin_id = ", i)))
+  
+  execGRASS("v.to.rast", flags = c("overwrite"), parameters = list(input = paste0("tmp_", i),
+                                                                   output = paste0("tmp_", i),
+                                                                   type = "area",
+                                                                   layer = "1",
+                                                                   use = "val",
+                                                                   value = 1))
+  
+  execGRASS("r.stream.basins", flags = "overwrite", parameters = list(direction = "dem_drain",
+                                                                      stream_rast = paste0("tmp_", i),
+                                                                      basins = paste0("tmp_", i, "_basin"),
+                                                                      memory = 4000))
+  
+  execGRASS("r.to.vect", flags = c("s", "overwrite"), parameters = list(input = paste0("tmp_", i, "_basin"),
+                                                                        output = paste0("tmp_", i, "_basin"),
+                                                                        type = "area"))
+}
+
+#Read lake basins into R and write to gis database
+lake_basin_list <- vector("list", length = length(lake_basin_indexes))
+for(i in seq_along(lake_basin_indexes)){
+  grass_vect <- tryCatch(readVECT(paste0("tmp_", lake_basin_indexes[i], "_basin")),
+                         error = function(e){NULL}) #Basin for two lakes cannot be delineated
+  if(is.null(grass_vect)){
+    lake_basin_list[[i]] <- grass_vect
+  }else{
+    lake_basin_list[[i]] <- grass_vect %>% 
+      st_as_sf() %>% 
+      add_column(lake_basin_id = lake_basin_indexes[i])
+  }
+}
+
+lake_basin_sf <- do.call(rbind, lake_basin_list) %>% 
+  select(lake_basin_id) %>% 
+  group_by(lake_basin_id) %>% 
+  summarise() %>% 
+  st_cast("MULTIPOLYGON") %>% 
+  left_join(st_drop_geometry(dk_lakes_sub)) %>% 
+  st_set_crs(dk_epsg)
+
+st_write(lake_basin_sf, dsn = gis_database, layer = "lake_basins", delete_layer = TRUE)
+
+#Clean grass database for temporary files
+#execGRASS("g.remove", flags = "f", parameters = list(type = "raster", pattern = "tmp_*"))
+#execGRASS("g.remove", flags = "f", parameters = list(type = "vector", pattern = "tmp_*"))
+
 
